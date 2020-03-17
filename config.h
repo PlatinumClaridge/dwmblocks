d4ef9d332b2fa0d40e7d44ce7bf4ec4e0b458163
Adrian Claridge <am@claridge.xyz>
2020-03-17 14:15:21 -0300

    minor module reorder

--- config.h
+++ config.h
@@ -3,9 +3,9 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"", "cat /tmp/recordingicon",	0,	9},
 	{"",	"music",	0,	11},
+	{"",	"mailbox",	180,	12},
 	{"",	"news",		0,	6},
 	/*{"",	"weather",	18000,	5},*/
-	{"",	"mailbox",	180,	12},
 	{"",	"volume",	0,	10},
 	/*{"",	"battery",	5,	0},*/
 	{"",	"clock",	60,	0},
