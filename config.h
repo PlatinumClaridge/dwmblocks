dbee2d4cb19b971bc52c7647ee263318f5e02b50
Adrian Claridge <61330144+PlatinumClaridge@users.noreply.github.com>
2020-03-27 14:38:58 -0300

    weather and internet on by default

--- config.h
+++ config.h
@@ -5,10 +5,11 @@ static const Block blocks[] = {
 	{"",	"music",	0,	11},
 	{"",	"mailbox",	180,	12},
 	{"",	"news",		0,	6},
-	/*{"",	"weather",	18000,	5},*/
+	{"",	"weather",	18000,	5},
 	{"",	"volume",	0,	10},
 	/*{"",	"battery | tr \'\n\' \' \'",	5,	0},*/
 	{"",	"clock",	60,	0},
+	{"",	"internet",	5,	4},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
