998f17e77235c315c6e62b10af62e4b96d0e4b02
Adrian Claridge <61330144+PlatinumClaridge@users.noreply.github.com>
2020-08-02 17:17:26 -0300

    Disabled mailbox by default

--- config.h
+++ config.h
@@ -3,7 +3,7 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
 	{"",	"music",	0,	11},
-	{"",	"mailbox",	180,	12},
+	/*{"",	"mailbox",	180,	12},*/
 	{"",	"news",		0,	6},
 	/*{"",	"weather",	18000,	5},*/
 	{"",	"volume",	0,	10},
